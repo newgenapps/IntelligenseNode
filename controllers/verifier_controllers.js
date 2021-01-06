b2b8b0cb3946add52ae681160c02530da6291497
Swati Srivastava <swati.srivastava@newgenapps.com>
2021-01-06 10:22:09 +0530

    Bulk email Verifier module added.

--- controllers/verifier_controllers.js
+++ controllers/verifier_controllers.js
@@ -1,6 +1,23 @@
 
 
 const EmailValidator = require("./../node_modules/email-deep-validator")
+const verify = require("./../node_modules/bulk-email-verifier")
+
+var domain = 'gmail.com';
+var emails =  [
+    'test3@gmail.com',
+    'test1@gmail.com',
+    'swatisrivs.13@gmail.com'
+];
+
+var domains = [
+  'skyfut2.com',
+  'yahoomail.com', 
+  'yahoo.com', 
+  'gmail.com',
+  'abc2.com',
+  'rediff.com'
+];
 
 const saySomething = (req, res, next) => {
   console.log("say something request response");
@@ -9,16 +26,49 @@ const saySomething = (req, res, next) => {
     });
 };
 
-//Verifies email Address
+//Verifies single email Address
 const verifier = async(req, res, next) => {
+ 
   const emailValidator = new EmailValidator();
+
+  let value1 = [];
   const { wellFormed, validDomain, validMailbox } = await emailValidator.verify(req.body.post);
-  console.log('response received');
+
+  res.send(
+    
+    `wellformed = ${wellFormed}, validDomain = ${validDomain}, validMailbox = ${validMailbox}`,
+
+    )
+  }
+  
+
+  //Verifies bulk domains
+const bulkDomainVerifier = async(req, res, next) => {
+  
+  verify.verifyDomainsMX(domains).then(function(res) {
+    console.log('Domains Status: ', res);
+});
   res.send(
 
     `welllformed = ${wellFormed}, validDomain = ${validDomain}, validMailbox = ${validMailbox}`,
     )
   }
   
+  //Verifies bulk emails
+const bulkEmailVerifier = async(req, res, next) => {
+  
+   verify.verifyEmails(domain, emails, {}, function(err, data){
+      console.log("Email Stats: ", err, data);  
+  });
+
+  res.send(
+
+    `welllformed = ${wellFormed}, validDomain = ${validDomain}, validMailbox = ${validMailbox}`,
+    )
+  }
+  
+
 module.exports.saySomething = saySomething;
 module.exports.verifier = verifier;
+module.exports.bulkEmailVerifier = bulkEmailVerifier;
+module.exports.bulkDomainVerifier = bulkDomainVerifier;
