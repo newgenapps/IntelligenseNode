656b662441fb8839749378bef76cdb0f3c877607
Swati Srivastava <swati.srivastava@newgenapps.com>
2021-01-08 17:49:50 +0530

    route updated

--- routes/tech-finder_routes.js
+++ routes/tech-finder_routes.js
@@ -2,6 +2,6 @@ const express = require('express');
 const router = express.Router();
 const controllers = require('./../controllers/tech-finder_controllers');
 
-router.post('/technologySearch', controllers.technologySearch);
+router.post('/tech-Search', controllers.technologySearch);
 
 module.exports = router;
