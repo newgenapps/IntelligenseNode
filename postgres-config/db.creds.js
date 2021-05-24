1a2aff17c26313ba92ca60832bc88505e8309430
Swati Srivastava <swati.srivastava@newgenapps.com>
2021-05-24 11:43:01 +0530

    Postgres credentials updated

--- postgres-config/db.creds.js
+++ postgres-config/db.creds.js
@@ -1,6 +1,6 @@
 const user = 'dbmasteruser'
-const host = 'ls-d1c1a3549a1acc691e5dbbeac5c67f3c17247444.c0xqlx0chwjl.us-east-1.rds.amazonaws.com'
-const password = 'wN`|B2}v00P>_K.Qfk5SWQ;S(t}mXO+M'
+const host = 'ls-bb85ecacfba9a943c9d7f003f61fd7c5dda3e25a.cegfsybufnwp.us-east-1.rds.amazonaws.com'
+const password = '?5PAVaW#*lGWwY6.-yc(a6A!oT&8{zRa'
 const port = 5432
 const database = 'Organization'
 
