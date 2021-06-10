e487773f5e292102e27445c6c39ca218cb2dc45a
Sundaram-Srivastava04 <sundaram.srivastava@newgenapps.com>
2021-06-10 12:59:16 +0530

    new db added

--- postgres-config/db.creds.js
+++ postgres-config/db.creds.js
@@ -1,8 +1,8 @@
-const user = 'dbmasteruser'
-const host = 'ls-bb85ecacfba9a943c9d7f003f61fd7c5dda3e25a.cegfsybufnwp.us-east-1.rds.amazonaws.com'
-const password = '?5PAVaW#*lGWwY6.-yc(a6A!oT&8{zRa'
+const user = 'postgres'
+const host = 'database-1.cleo4hipj2p8.us-east-1.rds.amazonaws.com'
+const password = 'intelligense'
 const port = 5432
-const database = 'Organization'
+const database = 'intelligense'
 
 module.exports = {
     user,
